af7f8318e15fbd8d20611294e11a82005c161788
SUN-1995 <sunbo123@mail.ustc.edu.cn>
2019-04-08 17:01:03 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,35 +1,13 @@
 #include <unistd.h>
 #include <pthread.h>
-#include<bits/stdc++.h>
-using namespace std;
-void someFuncMayExit()
-{
-    exit(1);
-}
-class GlobalObject
-{
-  public:
-    void doit()
-    {
-        pthread_mutex_lock(&mutex);
-        someFuncMayExit();
-        pthread_mutex_unlock(&mutex);
-    }
-    ~GlobalObject()
-    {
-        printf("GlobalObject:~GlobalObject\n");
-        pthread_mutex_lock(&mutex);
-        pthread_mutex_unlock(&mutex);
-        printf("GlobalObject:~GlobalObject cleanning\n");
-    }
+#include <iostream>
+#include <queue>
 
-  private:
-    pthread_mutex_t mutex;
-};
-GlobalObject g_obj;
-int main(int argvc,char *argv[])
+
+using namespace std;
+
+int main(int argvc, char *argv[])
 {
-    //g_obj.doit();
-    cout<<sizeof(int64_t);
+    
     return 0;
 }
